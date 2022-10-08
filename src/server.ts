baaa9e2af3f499d9e4d3998c86f1f1fdb4fdc2a9
wymbliathomazzoni <wymbliathomazzoni@acad.ftec.com.br>
2022-10-08 18:42:28 -0300

    ajustes para heroku

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { router } from "./routes"
-import cors from 'cors'
+import cors from "cors"
 
 const app = express()
 
@@ -8,4 +8,4 @@ app.use(express.json())
 app.use(cors())
 app.use(router)
 
-app.listen(4003, () => console.log("Server is runnin in port 4003"))
+app.listen(process.env.PORT || 4003, () => console.log("Server is runnin in port 4003"))
